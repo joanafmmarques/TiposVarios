5e19eeba39d6147a51847de6b8333f80dff8d2e5
Joana Marques <joana.pipaa@gmail.com>
2018-02-22 16:44:22 +0000

    Add a program

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Um tab\t e um copyright\u00A9";
+            string c = @"Nao e’ um codigo Unicode \u1234, nao e’ nova linha \n";
+            Console.WriteLine(s);
+            Console.WriteLine(c);
+        }
+    }
+}
